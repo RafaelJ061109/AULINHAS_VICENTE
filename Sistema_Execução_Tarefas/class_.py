cf8a99f172db11ed43d7612550b3a59c61ddf3b6
RafaelJ061109 <joaquim.rafael@escolar.ifrn.edu.br>
2026-04-09 18:02:18 -0300

    Implement task management menu and functionality
    Added a task management menu and functionality to create and list tasks.

--- Sistema_Execução_Tarefas/class_.py
+++ Sistema_Execução_Tarefas/class_.py
@@ -1,38 +1,33 @@
-tarefas = []
+from class_ import Tarefa, tarefas, Tarefa_Alternada, Tarefa_limitada
+#Sistema de Execução de Tarefas
 
-class Tarefa:
-    def __init__(self, tipo, nome, descricao):
-        self.tipo = tipo
-        self.nome = nome
-        self.descricao = descricao
-
-    def add_tarefa(self):
-        contador_comum = 0
-        if not tarefa_creatore in tarefas:
-            contador_comum =+ 1
-            tarefas.append(tarefa_creatore) 
-
-    def rmv_tarefa(self):
-        if tarefa_creatore in tarefas:
-            tarefas.remove(tarefa_creatore)
+#Menu
+while True:
+    print(" ==== SISTEMA DE TAREFAS ==== ") 
+    print("1 - Criar tarefa") 
+    print("2 - Listar tarefas") 
+    print("3 - Remover tarefa") 
+    print("0 - Sair") 
+    
+    opcao = input("Escolha uma opção: ")
+##Opções do Menu
+    if opcao == '0':
+        print("Compreensível, Tenha uma ótima vida")
+        break
+    elif opcao == '1':
+        print("Tipo de tarefa:")
+        print("1 - Comum")  
+        print("2 - Limitada")  
+        print("3 - Alternada")
+        tarefa_creatore = input("Opção: ")
+        if tarefa_creatore == '1':
+            tipo = 'Comum'
+            nome = input('Digite o nome da sua tarefa: ')
+            comum = input('Digite a descrição da sua tarefa: ').strip()
+            tarefa = Tarefa(tipo, nome, comum)
+            tarefa.add_tarefa()
         
-    def __str__(self):
-        return f'Tarefa: {self.tipo}, nome: {self.nome} Descrição: {self.descricao}'
-
-class Tarefa_limitada(Tarefa):
-    def __init__(self, nome, tipo, descricao, limite):
-        super().__init__(add_tarefa, rmv_tarefa)
-        self.limite = limite
-        
-        def limite(self, limite):
-            self.limite += 0
-
-class Tarefa_Alternada(Tarefa):
-    def __init__(self, nome, tipo, descricao, ativa):
-        super.__init__(add_tarefa, rmv_tarefa)
-        self.ativa = True
-
-
-
-
-
+    elif opcao == '2':
+        print("Aqui estão suas tarefas:")
+        for tarefa in tarefas:
+                print(tarefa)
