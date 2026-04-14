a9875c583446c0e9586d24e922be1b50db12e046
RafaelJ061109 <joaquim.rafael@escolar.ifrn.edu.br>
2026-04-14 15:20:54 -0300

    Update class_.py

--- Sistema_Execução_Tarefas/class_.py
+++ Sistema_Execução_Tarefas/class_.py
@@ -3,18 +3,16 @@ tarefas = []
 class Tarefa:
     contador = 1
 
-    def __init__(self, tipo, nome, descricao):
+    def __init__(self, tipo, nome, descricao, exe):
         self.id = Tarefa.contador
-        Tarefa.contador += 1 
+        Tarefa.contador += 1
         self.tipo = tipo
         self.nome = nome
         self.descricao = descricao
+        self.exe = exe
 
     def add_tarefa(self):
-        if not self in tarefas:
-            tarefas.append(self) 
-        else:
-            print('Tarefa já existente')
+        tarefas.append(self)
 
     def remover_por_id(id_tarefa):
         for tarefa in tarefas:
@@ -24,32 +22,38 @@ class Tarefa:
                 return
         print("Tarefa não encontrada.")
 
+    def executar(self):
+        print(f"[Tarefa {self.id}] {self.exe}")
+
     def __str__(self):
-        return f'ID: [{self.id}], Tarefa: {self.tipo}, nome: {self.nome} Descrição: {self.descricao}'
+        return f'ID: [{self.id}] | Tipo: {self.tipo} | Nome: {self.nome} | Descrição: {self.descricao}'
+
 
 class Tarefa_limitada(Tarefa):
-    def __init__(self, nome, tipo, descricao, limite):
-        super().__init__(nome, tipo, descricao)
+    def __init__(self, tipo, nome, descricao, limite, exe):
+        super().__init__(tipo, nome, descricao, exe)
         self.limite = limite
-        
-        def limite(self):
-            if self.limite >= limite:
-                print('Tarefa não pode ser mais executada.') 
+        self.execucoes = 0
 
-        def __str__(self):
-            return f'ID: [{self.id}], Tarefa: {self.tipo}, nome: {self.nome} Descrição: {self.descricao}, Limite: {self.limite}'
-
-class Tarefa_Alternada(Tarefa):
-    def __init__(self, nome, tipo, descricao, ativa):
-        super().__init__(nome, tipo, descricao)
-        self.ativa = True
+    def executar(self):
+        if self.execucoes < self.limite:
+            self.execucoes += 1
+            print(f"[Tarefa {self.id}] {self.exe} ({self.execucoes}/{self.limite})")
+        else:
+            print("Limite ultrapassado.")
 
     def __str__(self):
-        return f'ID: [{self.id}], Tarefa: {self.tipo}, nome: {self.nome} Descrição: {self.descricao}, Ativa: {self.ativa}'
+        return f'ID: [{self.id}] | Tipo: {self.tipo} | Nome: {self.nome} | Limite: {self.limite}'
 
 
-    def __init__(self, nome, tipo, descricao, ativa):
-        super().__init__(nome, tipo, descricao)
-        self.ativa = True
+class Tarefa_Alternada(Tarefa):
+    def __init__(self, tipo, nome, descricao, ativa, exe):
+        super().__init__(tipo, nome, descricao, exe)
+        self.ativa = True if ativa.lower() == 's' else False
 
+    def executar(self):
+        estado = "ATIVA" if self.ativa else "INATIVA"
+        print(f"[Tarefa {self.id}] {self.exe} ({estado})")
 
+    def __str__(self):
+        return f'ID: [{self.id}] | Tipo: {self.tipo} | Nome: {self.nome} | Ativa: {self.ativa}'
